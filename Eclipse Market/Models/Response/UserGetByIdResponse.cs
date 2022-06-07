3378ea8a5617159f9a89c350d46f43d25a322a8b
Daniel Dobrev <danidobrev2005@gmail.com>
2022-06-07 20:22:42 +0300

    Some changes.

--- Eclipse Market/Models/Response/UserGetByIdResponse.cs
+++ Eclipse Market/Models/Response/UserGetByIdResponse.cs
@@ -10,9 +10,9 @@ namespace Eclipse_Market.Models.Response
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-        public IEnumerable<ListingGetAllResponse> BookmarkedListings { get; set; }
-        public IEnumerable<ListingGetAllResponse> CurrentListings { get; set; }
-        public IEnumerable<MessageGetAllResponse> Messages { get; set; }
         public int RoleId { get; set; }
+        public IEnumerable<ListingGetAllResponse> CurrentListings { get; set; }
+        public IEnumerable<ListingGetAllResponse> BookmarkedListings { get; set; }
+        public IEnumerable<MessageGetAllResponse> Messages { get; set; }
     }
 }
