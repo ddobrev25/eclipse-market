54db842878dd00c60a0a8d5489cb394dc1945722
Georgi <96887145+rammer17@users.noreply.github.com>
2022-10-12 13:42:24 +0300

    added auth guard + proper form error response

--- Eclipse Market/ClientApp/src/app/app-routing.module.ts
+++ Eclipse Market/ClientApp/src/app/app-routing.module.ts
@@ -3,27 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './core/guards/auth.guard.service';
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'auth', component: AuthComponent },
+  { path: 'auth', component: AuthComponent, canActivate: [AuthGuardService] },
   {
-    path: "account",
+    path: 'account',
     loadChildren: () =>
-      import("./accounts/account.module").then(m => m.AccountModule)
+      import('./accounts/account.module').then((m) => m.AccountModule),
   },
-  { 
-    path: 'listings',     
+  {
+    path: 'listings',
     loadChildren: () =>
-      import("./listings/listings.module").then(m => m.ListingModule) 
+      import('./listings/listings.module').then((m) => m.ListingModule),
   },
   { path: '404', component: ErrorPageComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Eclipse Market/ClientApp/src/app/core/guards/auth.guard.service.ts
+++ Eclipse Market/ClientApp/src/app/core/guards/auth.guard.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class AccountGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate {
   constructor(private router: Router, private jwtHelper: JwtHelperService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log('entered guard');
+
     const token = localStorage.getItem('token');
     if (token && !this.jwtHelper.isTokenExpired(token)) {
-      return true;
+        console.log('entered if');
+        this.router.navigate(['/account/info']);
+        return false;
     }
+
     this.router.navigate(['/auth']);
     return false;
   }
+  
 }
