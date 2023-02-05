1b5e712c18a402fd9e39653d1756f14a450a79bf
Georgi <georgi.rosenov17@gmail.com>
2023-02-05 11:57:00 +0200

    bug fixes

--- Eclipse Market/ClientApp/src/app/shared/shared.module.ts
+++ Eclipse Market/ClientApp/src/app/shared/shared.module.ts
@@ -1,29 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { ToastModule } from 'primeng/toast';
-import { ToolbarModule } from 'primeng/toolbar';
-import { FormErrorsComponent } from './form-errors/form-errors.component';
-import { ClaimAccessDirective } from '../core/directives/claim-access.directive';
-import { StepsModule } from 'primeng/steps';
-import { CardModule } from 'primeng/card';
-import { TooltipModule } from 'primeng/tooltip';
-import { FileUploadModule } from 'primeng/fileupload';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { DataViewModule } from 'primeng/dataview';
-import { ChipModule } from 'primeng/chip';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { PasswordModule } from 'primeng/password';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { InputSwitchModule } from 'primeng/inputswitch';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BreadcrumbModule } from "primeng/breadcrumb";
+import { CascadeSelectModule } from "primeng/cascadeselect";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import { DialogModule } from "primeng/dialog";
+import { DropdownModule } from "primeng/dropdown";
+import { InputTextModule } from "primeng/inputtext";
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { ToolbarModule } from "primeng/toolbar";
+import { FormErrorsComponent } from "./form-errors/form-errors.component";
+import { ClaimAccessDirective } from "../core/directives/claim-access.directive";
+import { StepsModule } from "primeng/steps";
+import { CardModule } from "primeng/card";
+import { TooltipModule } from "primeng/tooltip";
+import { FileUploadModule } from "primeng/fileupload";
+import { TabMenuModule } from "primeng/tabmenu";
+import { DataViewModule } from "primeng/dataview";
+import { ChipModule } from "primeng/chip";
+import { ProgressBarModule } from "primeng/progressbar";
+import { PasswordModule } from "primeng/password";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { InputSwitchModule } from "primeng/inputswitch";
+import { ToggleButtonModule } from "primeng/togglebutton";
 
 @NgModule({
   declarations: [FormErrorsComponent, ClaimAccessDirective],
@@ -52,6 +53,7 @@ import { InputSwitchModule } from 'primeng/inputswitch';
     InputTextareaModule,
     AutoCompleteModule,
     InputSwitchModule,
+    ToggleButtonModule,
   ],
 
   exports: [
@@ -81,6 +83,7 @@ import { InputSwitchModule } from 'primeng/inputswitch';
     InputTextareaModule,
     AutoCompleteModule,
     InputSwitchModule,
+    ToggleButtonModule,
   ],
 })
 export class SharedModule {}
