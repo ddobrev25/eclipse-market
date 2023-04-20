aba3c2b809758debb1e18d47dc18ba7c9832dccb
Georgi <georgi.rosenov17@gmail.com>
2023-04-20 15:34:18 +0300

    removed EclipseMarket folder

--- Eclipse Market/ClientApp/src/app/core/models/admin.model.ts
+++
@@ -1,7 +0,0 @@
-import { ListingCategoryGetAllResponse } from "./listing-category.model";
-import { RoleGetAllResponse } from "./role.model";
-import { UserGetAllResponse } from "./user.model";
-
-export type AdminDataUsers$ = UserGetAllResponse | null;
-export type AdminDataRoles$ = RoleGetAllResponse | null;
-export type AdminDataCategories$ = ListingCategoryGetAllResponse | null;
